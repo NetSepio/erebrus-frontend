f8fc79e74ca3256dbc8d9f20fa5457e99a3614c8
Devsi <grace.emily876@gmail.com>
2024-05-04 16:37:30 +0530

    ui fixes

--- components/Navbar.js
+++ components/Navbar.js
@@ -451,7 +451,7 @@ const Navbar = ({ isHome }) => {
     );
 
     if (!jwt_token_encoded) {
-      createRuntimeError("Could not retrieve a valid JWT Token!");
+      // createRuntimeError("Could not retrieve a valid JWT Token!");
       return;
     }
 
@@ -600,76 +600,38 @@ const Navbar = ({ isHome }) => {
 
           {!token && !userAddress && (<button className="text-white" onClick={()=>setloginoptions(true)}>Login</button>)}
 
-          {/* <div className='flex mt-4 mb-10 space-x-4 justify-center'>
-          
-              <button onClick={redirectlogin} className='bg-white text-gray-700 hover:text-gray-900 font-semibold py-2 px-4 border rounded-lg flex items-center space-x-2'>
-                <svg
-                  xmlns='http://www.w3.org/2000/svg'
-                  x='0px'
-                  y='0px'
-                  width='48'
-                  height='48'
-                  viewBox='0 0 48 48'
-                >
-                  <path
-                    fill='#FFC107'
-                    d='M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12c0-6.627,5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24c0,11.045,8.955,20,20,20c11.045,0,20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z'
-                  ></path>
-                  <path
-                    fill='#FF3D00'
-                    d='M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z'
-                  ></path>
-                  <path
-                    fill='#4CAF50'
-                    d='M24,44c5.166,0,9.86-1.977,13.409-5.192l-6.19-5.238C29.211,35.091,26.715,36,24,36c-5.202,0-9.619-3.317-11.283-7.946l-6.522,5.025C9.505,39.556,16.227,44,24,44z'
-                  ></path>
-                  <path
-                    fill='#1976D2'
-                    d='M43.611,20.083H42V20H24v8h11.303c-0.792,2.237-2.231,4.166-4.087,5.571c0.001-0.001,0.002-0.001,0.003-0.002l6.19,5.238C36.971,39.205,44,34,44,24C44,22.659,43.862,21.35,43.611,20.083z'
-                  ></path>
-                </svg>
-                <span>Login with Google</span>
-              </button>
-          </div> */}
-
 {userAddress ? (
-            <div className='px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0'>
-              <dt className='text-sm font-medium leading-6 text-gray-900'>
-                User Address
-              </dt>
-              <dd className='mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0'>
-                <span className='mr-5'>{userAddress}</span>
-                <span className='ml-0'>
+            <div className='mr-2'>
+              <dd className='mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 text-white'>
+                <div>{userAddress?.slice(0, 3)}...{userAddress?.slice(-3)}</div>
+                <div className="flex justify-center">
                   <button
                     type='button'
-                    className='rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50'
+                    className='rounded-md bg-white px-2 py-1 text-xs font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50'
                     onClick={() => {
                       navigator.clipboard.writeText(userAddress);
                     }}
                   >
                     Copy
                   </button>
-                </span>
+                </div>
               </dd>
             </div>
           ) : null}
           {userAddress ? (
-            <div className='px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0'>
-              <dt className='text-sm font-medium leading-6 text-gray-900'>
-                Balance
-              </dt>
-              <dd className='mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0'>
-                <span className='mr-5'>{userBalance.toFixed(4)} SUI</span>
-                <span className='ml-5'>
+            <div className=''>
+              <dd className='mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0 text-white'>
+                <div className="justify-end flex">{userBalance.toFixed(4)} SUI</div>
+                <div>
                   <button
                     type='button'
-                    className='rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50'
+                    className='rounded-md bg-white py-1 px-2 text-xs font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50'
                     disabled={!userAddress}
                     onClick={() => giveSomeTestCoins(userAddress)}
                   >
                     Get Testnet Coins
                   </button>
-                </span>
+                </div>
               </dd>
             </div>
           ) : null}
@@ -695,41 +657,6 @@ const Navbar = ({ isHome }) => {
               )}
             </div>
           )}
-
-          {/* {!token ? (
-            <div className='lg:mt-0 mt-4 z-50 rounded-xl text-white'>
-              <ConnectButton label='connect' />
-
-              {status === "connected" && showsignbutton && (
-                <Button
-                  color={"blue"}
-                  onClick={onSignMessage}
-                  disabled={false}
-                  message={"Authenticate"}
-                />
-              )}
-            </div>
-          ) : (
-            <div
-              className='lg:mt-0 mt-4 z-50 rounded-xl flex gap-4'
-              style={{ color: "#0162FF" }}
-            >
-              <button
-                onClick={handleDeleteCookie}
-                onMouseOver={(e) =>
-                  (e.currentTarget.style.borderBottom = "1px solid #0162FF")
-                }
-                onMouseOut={(e) =>
-                  (e.currentTarget.style.borderBottom = "none")
-                }
-              >
-                Log out
-              </button>
-              {avatarUrl && (
-                <img src={avatarUrl} alt='Avatar' className='w-10 ml-auto' />
-              )}
-            </div>
-          )} */}
         </div>
         <div className='block lg:hidden'>
           <button
