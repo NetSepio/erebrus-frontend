1201e0159582923f8b2e1a06eed171bd542d9650
Devsi <grace.emily876@gmail.com>
2024-05-04 14:58:17 +0530

    login popup option

--- components/Navbar.js
+++ components/Navbar.js
@@ -49,6 +49,7 @@ const Navbar = ({ isHome }) => {
   const [link, setlink] = useState("");
   const { isSignedIn, setIsSignedIn } = useContext(AuthContext);
   const [avatarUrl, setAvatarUrl] = useState("");
+  const [loginoptions, setloginoptions] = useState(false);
   // const sdk = useSDK();
 
   const { status, connected, connecting, account, network, name } = useWallet();
@@ -455,7 +456,9 @@ const Navbar = ({ isHome }) => {
             Docs
           </Link>
 
-          <div className='flex mt-4 mb-10 space-x-4 justify-center'>
+          <button className="text-white" onClick={()=>setloginoptions(true)}>Login</button>
+
+          {/* <div className='flex mt-4 mb-10 space-x-4 justify-center'>
           
               <button onClick={redirectlogin} className='bg-white text-gray-700 hover:text-gray-900 font-semibold py-2 px-4 border rounded-lg flex items-center space-x-2'>
                 <svg
@@ -485,9 +488,31 @@ const Navbar = ({ isHome }) => {
                 </svg>
                 <span>Login with Google</span>
               </button>
-          </div>
+          </div> */}
 
-          {!token ? (
+{token && (
+            <div
+              className='lg:mt-0 mt-4 z-50 rounded-xl flex gap-4'
+              style={{ color: "#0162FF" }}
+            >
+              <button
+                onClick={handleDeleteCookie}
+                onMouseOver={(e) =>
+                  (e.currentTarget.style.borderBottom = "1px solid #0162FF")
+                }
+                onMouseOut={(e) =>
+                  (e.currentTarget.style.borderBottom = "none")
+                }
+              >
+                Log out
+              </button>
+              {avatarUrl && (
+                <img src={avatarUrl} alt='Avatar' className='w-10 ml-auto' />
+              )}
+            </div>
+          )}
+
+          {/* {!token ? (
             <div className='lg:mt-0 mt-4 z-50 rounded-xl text-white'>
               <ConnectButton label='connect' />
 
@@ -505,9 +530,6 @@ const Navbar = ({ isHome }) => {
               className='lg:mt-0 mt-4 z-50 rounded-xl flex gap-4'
               style={{ color: "#0162FF" }}
             >
-              {/* <div>
-                {address?.slice(0, 4)}...{address?.slice(-4)}
-              </div> */}
               <button
                 onClick={handleDeleteCookie}
                 onMouseOver={(e) =>
@@ -523,7 +545,7 @@ const Navbar = ({ isHome }) => {
                 <img src={avatarUrl} alt='Avatar' className='w-10 ml-auto' />
               )}
             </div>
-          )}
+          )} */}
         </div>
         <div className='block lg:hidden'>
           <button
@@ -548,6 +570,100 @@ const Navbar = ({ isHome }) => {
           </button>
         </div>
       </div>
+
+      { loginoptions && (
+
+<div
+          style={{ backgroundColor: "#222944E5" }}
+          className="flex overflow-y-auto overflow-x-hidden fixed inset-0 z-50 justify-center items-center w-full max-h-full"
+          id="popupmodal"
+        >
+          <div className="relative p-4 lg:w-1/3 w-full max-w-2xl max-h-full">
+            <div className="relative rounded-lg shadow bg-black text-white">
+              <div className="flex items-center justify-end p-4 md:p-5 rounded-t dark:border-gray-600">
+                <button
+                  onClick={() => setloginoptions(false)}
+                  type="button"
+                  className="text-white bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                >
+                  <svg
+                    className="w-3 h-3"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 14 14"
+                  >
+                    <path
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+                    />
+                  </svg>
+                  <span className="sr-only">Close modal</span>
+                </button>
+              </div>
+
+              {/* <Image src={emoji} alt="info" className="mx-auto"/> */}
+
+              <div className="p-4 space-y-4">
+                <p className="text-3xl text-center font-bold text-green-500">
+                Login Options
+                </p>
+              </div>
+              <div className="flex justify-center p-4 pb-10">
+              {!token ? (
+            <div className='lg:mt-0 mt-4 z-50 rounded-xl text-white'>
+              <ConnectButton label='connect' />
+
+              {status === "connected" && showsignbutton && (
+                <Button
+                  color={"blue"}
+                  onClick={onSignMessage}
+                  disabled={false}
+                  message={"Authenticate"}
+                />
+              )}
+            </div>
+          ) : (
+            <></>
+          )}
+                    </div>
+                    <div className="flex justify-center p-4 pb-20">
+                    <button onClick={redirectlogin} className='bg-white text-gray-700 hover:text-gray-900 font-semibold py-2 px-4 border rounded-lg flex items-center space-x-2'>
+                <svg
+                  xmlns='http://www.w3.org/2000/svg'
+                  x='0px'
+                  y='0px'
+                  width='48'
+                  height='48'
+                  viewBox='0 0 48 48'
+                >
+                  <path
+                    fill='#FFC107'
+                    d='M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12c0-6.627,5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24c0,11.045,8.955,20,20,20c11.045,0,20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z'
+                  ></path>
+                  <path
+                    fill='#FF3D00'
+                    d='M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z'
+                  ></path>
+                  <path
+                    fill='#4CAF50'
+                    d='M24,44c5.166,0,9.86-1.977,13.409-5.192l-6.19-5.238C29.211,35.091,26.715,36,24,36c-5.202,0-9.619-3.317-11.283-7.946l-6.522,5.025C9.505,39.556,16.227,44,24,44z'
+                  ></path>
+                  <path
+                    fill='#1976D2'
+                    d='M43.611,20.083H42V20H24v8h11.303c-0.792,2.237-2.231,4.166-4.087,5.571c0.001-0.001,0.002-0.001,0.003-0.002l6.19,5.238C36.971,39.205,44,34,44,24C44,22.659,43.862,21.35,43.611,20.083z'
+                  ></path>
+                </svg>
+                <span>Login with Google</span>
+              </button>
+              </div>
+            </div>
+          </div>
+        </div>
+  )}
 
       <motion.nav animate={isOpen ? "open" : "closed"} variants={variants}>
         {isOpen && (
