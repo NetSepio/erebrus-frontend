ca8de07a4ff649f8d6a05c4335ac3efd860118bc
Rushikesh Nimkar <rushikeshnimkar396@gmail.com>
2024-05-11 21:22:38 +0530

    build fix

--- components/AppContext.tsx
+++ components/AppContext.tsx
@@ -1,8 +1,7 @@
 "use client"
-import { useWallet, ConnectModal } from '@suiet/wallet-kit';
 import {WalletProvider} from '@suiet/wallet-kit';
 import '@suiet/wallet-kit/style.css';
-import { clusterApiUrl } from "@solana/web3.js";
+
 import { FC, ReactNode, useEffect, useMemo, useState } from "react";
 
 
