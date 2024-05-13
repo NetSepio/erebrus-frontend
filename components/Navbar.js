ff1ac0be2ede389f8e8d6682cb711f997529a93c
Rushikesh Nimkar <rushikeshnimkar396@gmail.com>
2024-05-14 01:16:53 +0530

    fixed  google login verification

--- components/Navbar.js
+++ components/Navbar.js
@@ -74,7 +74,7 @@ const Navbar = ({ isHome }) => {
   const { status, connected, connecting, account, network, name } = useWallet();
   const wallet = useWallet();
   let sendable = isSendableNetwork(status === "connected", wallet.chain.id);
-
+console.log(wallet)
   const router = useRouter();
   // console.log("router", router);
 
@@ -224,7 +224,7 @@ const Navbar = ({ isHome }) => {
   //         nonce: nonce,
   //       };
         async function handleSignMsg() {
-          if(sendable){
+          if(sendable && wallet.account?.publicKey.length !=0){
           try {
             const REACT_APP_GATEWAY_URL = process.env.NEXT_PUBLIC_GATEWAY_URL;
             const { data } = await axios.get(
@@ -239,7 +239,7 @@ const Navbar = ({ isHome }) => {
             const result = await wallet.signPersonalMessage({
               message: msgBytes
             })
-            console.log("signature",result.signature )
+            console.log("signature",result )
             console.log("publickey", wallet.account?.publicKey)
                   // verify signature with publicKey and SignedMessage (params are all included in result)
             const verifyResult = await wallet.verifySignedMessage(result, wallet.account.publicKey)
@@ -275,6 +275,7 @@ const Navbar = ({ isHome }) => {
         console.log("adaddasdasd", result.signature)
 
         const authenticateApiUrl = `${REACT_APP_GATEWAY_URL}api/v1.0/authenticate?chain=sui`;
+        // const authenticateApiUrl = `${REACT_APP_GATEWAY_URL}api/v1.0/authenticate/NonSign`;
 
         const config = {
           url: authenticateApiUrl,
@@ -301,6 +302,92 @@ const Navbar = ({ isHome }) => {
         setshowsignbutton(true);
       }
      
+    }
+    else if (sendable && wallet.account?.publicKey.length == 0 ){
+      try {
+        const REACT_APP_GATEWAY_URL = process.env.NEXT_PUBLIC_GATEWAY_URL;
+        const { data } = await axios.get(
+          `${REACT_APP_GATEWAY_URL}api/v1.0/flowid?walletAddress=${wallet.address}&chain=sui`
+        );
+        console.log("address", wallet.address)
+        const msg = data.payload.eula + data.payload.flowId;
+        const nonce = data.payload.flowId;
+        // convert string to Uint8Array 
+        const msgBytes = new TextEncoder().encode(msg)
+  
+        const result = await wallet.signPersonalMessage({
+          message: msgBytes
+        })
+        const byteArray = Uint8Array.from(atob(result.signature), c => c.charCodeAt(0));
+        console.log("signature array",byteArray)
+        console.log("signature",result )
+        console.log("publickey", wallet.account?.publicKey)
+              // verify signature with publicKey and SignedMessage (params are all included in result)
+        const verifyResult = await wallet.verifySignedMessage(result, wallet.account.publicKey)
+        if (!verifyResult) {
+          console.log('signPersonalMessage succeed, but verify signedMessage failed')
+        } else {
+          console.log('signPersonalMessage succeed, and verify signedMessage succeed!')
+        }
+      
+        const payload = {
+                  message: message,
+                  nonce: nonce,
+                };
+    
+  
+    //   const response = await signMessage(payload);
+    //   console.log(response);
+
+    //   let signaturewallet = response.signature;
+
+    // if(signaturewallet.length === 128)
+    // {
+    //   signaturewallet = `0x${signaturewallet}`;
+    // }
+  
+
+    const authenticationData = {
+      flowId: nonce,
+      "walletAddress":wallet.address, 
+     
+
+    };
+  
+    console.log("adaddasdasd", result.signature)
+  
+
+    // const authenticateApiUrl = `${REACT_APP_GATEWAY_URL}api/v1.0/authenticate?chain=sui`;
+    const authenticateApiUrl = `${REACT_APP_GATEWAY_URL}api/v1.0/authenticate/NonSign`;
+
+    const config = {
+      url: authenticateApiUrl,
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      data: authenticationData,
+    };
+
+    const authResponse = await axios(config);
+    console.log("auth data", authResponse.data);
+
+    const token = await authResponse?.data?.payload?.token;
+    const userId = await authResponse?.data?.payload?.userId;
+
+    Cookies.set("erebrus_token", token, { expires: 7 });
+    Cookies.set("erebrus_wallet", account?.address ?? "", { expires: 7 });
+    Cookies.set("erebrus_userid", userId, { expires: 7 });
+
+    window.location.reload();
+  } catch (error) {
+    console.error(error);
+    setshowsignbutton(true);
+  }
+
+    }
+    else {
+      alert(`Switch to Sui Testnet in your wallet`);
     }
     } 
   
